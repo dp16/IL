669e18bbd4edb70ace98825c1e7c3863d3c1b0ff
Sawyer Hollenshead <s.hollenshead@gmail.com>
2013-01-21 20:32:01 -0500

    Setup ad refresh

--- js/site.js
+++ js/site.js
@@ -3,6 +3,9 @@ jQuery(document).ready(function($) {
     var $content = $('#fullhome');
 
     $('#pagination a').on('click', function(e){
+        if(typeof writeCaptureRefresh == 'function'){
+            writeCaptureRefresh();
+        }
         $content.infinitescroll('retrieve');
         e.preventDefault();
     });
